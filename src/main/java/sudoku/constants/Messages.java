d01de35e23773a2018d8e40017fcf41b9dad0a60
timiiscariot <timiiscariot@hotmail.co.uk>
2021-05-07 02:08:34 +0200

    sudokuAppFirstSteps

---
+++ src/main/java/sudoku/constants/Messages.java
@@ -0,0 +1,10 @@
+package sudoku.constants;
+
+public class Messages {
+
+    public static final String GAME_COMPLETE = "Congratulations," +
+            "you have won! New Game?";
+    public static final String ERROR = "An error has occurred.";
+
+
+}
